83537aec98992af05e72eb992a0968ca2a782510
sharma-daksh <daksharma015@gmail.com>
2025-11-29 00:18:25 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
+import path from "path"
 import { ENV } from "./lib/env.js";
+
 const app = express();
+const __dirname = path.resolve()
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
-app.get("/", (req, res) => {
+app.get("/dss", (req, res) => {
   res.status(200).json({ msg: "success from backend" });
 });
 
+
+if(ENV.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("/{*any}",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+  })
+}
 app.listen(ENV.PORT, () => console.log("Server runing on port:",ENV.PORT));
