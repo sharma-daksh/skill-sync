806c6022628c0cfd30e3e89e9c21efafa1511f79
sharma-daksh <daksharma015@gmail.com>
2025-12-04 15:27:35 +0530

    Clean up invalid file deps

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import {serve} from "inngest/express"
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { inngest , functions } from "./lib/inngest.js";
 
 const app = express();
 const __dirname = path.resolve();
+// middleware
+app.use(express.json());
+// credentials:true => server allows browser to include cookies ON REQUEST 
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 app.get("/dss", (req, res) => {
   res.status(200).json({ msg: "success from backend" });
