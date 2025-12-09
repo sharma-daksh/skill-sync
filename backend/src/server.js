c00d844100508b682d68386f1432af22f48ce54a
sharma-daksh <daksharma015@gmail.com>
2025-12-09 06:27:36 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import {serve} from "inngest/express"
+import { clerkMiddleware } from '@clerk/express';
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest , functions } from "./lib/inngest.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -13,8 +15,10 @@ const __dirname = path.resolve();
 app.use(express.json());
 // credentials:true => server allows browser to include cookies ON REQUEST 
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(clerkMiddleware);
 
 app.use("/api/inngest",serve({client:inngest,functions}))
+app.use("/api/chat",chatRoutes);
 
 app.get("/dss", (req, res) => {
   res.status(200).json({ msg: "success from backend" });
