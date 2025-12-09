43c6d6ab7a1bd9559a01b5967e43e542b1d9831a
sharma-daksh <daksharma015@gmail.com>
2025-12-09 06:31:25 +0530

    typo fixes

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,18 +6,18 @@ export const protectRoute =[
     async (req,res, next)=>{
         try{
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized-Invalid token "});
+            if(!clerkId) return res.status(401).json({message: "Unauthorized-Invalid token "});
 
             //finde user by clerkId in datbase
             const user = await User.findOne({clerkId});
-            if(!user) return res.status(404).json({msg: "User not found"});
+            if(!user) return res.status(404).json({message: "User not found"});
 
 
             req.user=user;//attch user to req
             next();
         }catch(error){
             console.error("Error in protectRoute middleware",error);
-            resizeBy.status(500).json({msg:"Internal server error"});
+            resizeBy.status(500).json({message:"Internal server error"});
         }
     },
 ];
